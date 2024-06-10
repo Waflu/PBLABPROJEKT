b5b97028609a459567b8d1915e5cf54b1e1812b0
Artur Głodek <116422268+Waflu@users.noreply.github.com>
2024-06-10 16:10:20 +0200

    Add files via upload

---
+++ RazorPages/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPages.Data;
+using RazorPages.Models;
+
+namespace RazorPages.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPages.Data.RazorPagesContext _context;
+
+        public IndexModel(RazorPages.Data.RazorPagesContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
